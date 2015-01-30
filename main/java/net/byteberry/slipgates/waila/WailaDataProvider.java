c3269c94b52a235816b43ee4ba6c76fa046348a2
alu- <alu@byteberry.net>
2015-01-30 06:49:06 +0100

    Class stub for Waila integration

---
+++ main/java/net/byteberry/slipgates/waila/WailaDataProvider.java
@@ -0,0 +1,5 @@
+package net.byteberry.slipgates.waila;
+
+public class WailaDataProvider {
+	// TODO @see https://github.com/pahimar/Equivalent-Exchange-3/blob/bce498695e1454cc48c4263be9c0bb1ee063dbc5/src/main/java/com/pahimar/ee3/waila/WailaDataProvider.java
+}
