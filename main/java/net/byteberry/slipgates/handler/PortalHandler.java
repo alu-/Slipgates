68a7a88e8423b7bc65e24ddff7970228d76bc0e7
alu- <alu@byteberry.net>
2015-01-30 06:58:16 +0100

    Class keeping track of portals

--- main/java/net/byteberry/slipgates/handler/PortalHandler.java
+++ main/java/net/byteberry/slipgates/handler/PortalHandler.java
@@ -1,6 +1,108 @@
 package net.byteberry.slipgates.handler;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.lwjgl.util.vector.Vector3f;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldSavedData;
+import net.minecraft.world.storage.MapStorage;
+
 // Class in charge of keeping track on portal multiblock positions
 public class PortalHandler {
+	// TODO implement DataOutputStream as save mechanism
+	private HashMap dimensionToCoordinates = new HashMap<Integer, ArrayList<Vector3f>>();
+
+	public PortalHandler() {
+		// yay!
+
+		// Vector3f greger = new Vector3f(1,1,1);
+		// ArrayList test = new ArrayList<Vector3f>();
+		// test.add(greger);
+		// dimensionToCoordinates.put(0, test);
+	}
+
+	public void load() {
+		try {
+			FileInputStream fis = new FileInputStream("portals.dat");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			dimensionToCoordinates = (HashMap) ois.readObject();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void save() {
+		try {
+			FileOutputStream fos = new FileOutputStream("portals.dat");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(dimensionToCoordinates);
+			oos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void addPortal(World world, int x, int y, int z) {
+		Vector3f coords = new Vector3f(x, y, z);
+		Integer dimensionId = world.provider.dimensionId;
+
+	}
+
+	public void removePortal() {
+
+	}
+
+	public void getAllPortalsInDimension(Integer dimensionId) {
+		if (dimensionToCoordinates.containsKey(dimensionId)) {
+			ArrayList test = (ArrayList) dimensionToCoordinates.get(dimensionId);
+			Iterator<Vector3f> iterator = test.iterator();
+
+			while (iterator.hasNext()) {
+				Vector3f element = iterator.next();
+				System.out.print("Portal: " + element.toString());
+			}
+		}
+	}
+
+	public String getAllPortals() {
+		String portals = "";
+
+		Iterator hashIterator = dimensionToCoordinates.entrySet().iterator();
+		while (hashIterator.hasNext()) {
+			Map.Entry pairs = (Entry) hashIterator.next();
+
+			System.out.println(pairs.getKey() + " = " + pairs.getValue());
+		}
+
+//		Iterator<Vector3f> iterator = test.iterator();
+//		while (iterator.hasNext()) {
+//			Vector3f element = iterator.next();
+//			System.out.print("Portal: " + element.toString());
+//		}
+		return portals;
+	}
 
 }
